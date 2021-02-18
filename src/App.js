e674cc1d49af7e11e12313a7d20f13893bcf682e
Eunjung Nam <eunjungnam@eunjungs-air.lan>
2021-02-18 13:56:38 -0500

    Add weather info markup

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ const api = {
 };
 
 function App() {
+  const year = new Date().getFullYear();
+  const month = new Date().getMonth() + 1;
+  const day = new Date().getDate();
+  console.log(month, day, year);
+
   return (
     <div className="app">
       <main>
@@ -15,6 +20,18 @@ function App() {
             className="search-bar"
             placeholder="Search..."
           ></input>
+        </div>
+
+        <div className="location-box">
+          <div className="location">Toronto, Canada</div>
+          <div className="date">
+            {month}/{day}' {year}
+          </div>
+        </div>
+
+        <div className="weather-box">
+          <div className="temp">-15°C</div>
+          <div className="weather">Snowing</div>
         </div>
       </main>
     </div>
